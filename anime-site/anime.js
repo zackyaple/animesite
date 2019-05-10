383b931a09cb1a651a7221ca57aa68e3adbffed4
Zack Yaple <zack@yaple.net>
2019-05-10 13:43:00 -0400

    anime site works in progress

---
+++ anime-site/anime.js
@@ -0,0 +1,3 @@
+function Header() {
+  return <img src={images/cover1.jpg} alt="anime" />;
+}
---
+++ anime-site/new/player.js
@@ -0,0 +1,21 @@
+<script>
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+</script>
+<script>
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+</script>
+
